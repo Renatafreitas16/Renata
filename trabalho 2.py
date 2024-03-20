6e728dbab030c9c9e72230a8f80410dbe28c508d
Renatafreitas16 <159313923+Renatafreitas16@users.noreply.github.com>
2024-03-20 15:55:10 -0300

    Add files via upload

---
+++ trabalho 2.py
@@ -0,0 +1,33 @@
+print("Renata")
+
+numero = 10
+nome = "João"
+booleano = True
+
+numero_real = 10.0
+
+a = 20
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+c = True
+d = False
+resultado1 = c and d
+resultado2 = c or d
+resultado3 = not c
+
+e = 3
+e == 4
+e > 2
+e != 3
+
+f = 4
+e is f
+e is not f
+
+# a ordem de precedencia dos operadores logicos é: not, and e or.
+
+resposta = (a > b) or (e is not 8)
